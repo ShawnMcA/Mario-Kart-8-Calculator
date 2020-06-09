9cde80152a2ac49efc8dd9c2da76e82cedda1d65
ShawnMcA <Shawnmcallister39@gmail.com>
2020-06-08 20:45:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mario-Kart-8-Calculator
+ Application to calculate the stats for a given character/kart combination.
---
+++ data/driver.js
@@ -0,0 +1,222 @@
+export const drivers = [
+   {
+      "name": "Mario",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5090496be115c484806a"
+   },
+   {
+      "name": "Luigi",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5086496be115c4848069"
+   },
+   {
+      "name": "Peach",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5070496be115c4848067"
+   },
+   {
+      "name": "Daisy",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5070496be115c4848067"
+   },
+   {
+      "name": "Rosalina",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5097496be115c484806b"
+   },
+   {
+      "name": "Tanooki Mario",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5080496be115c4848068"
+   },
+   {
+      "name": "Cat Peach",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5069496be115c4848066"
+   },
+   {
+      "name": "Yoshi",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5070496be115c4848067"
+   },
+   {
+      "name": "Toad",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5062496be115c4848065"
+   },
+   {
+      "name": "Koopa Troopa",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a504a496be115c4848063"
+   },
+   {
+      "name": "Shy Guy",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5062496be115c4848065"
+   },
+   {
+      "name": "Lakitu",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a504a496be115c4848063"
+   },
+   {
+      "name": "Toadette",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a505a496be115c4848064"
+   },
+   {
+      "name": "King Boo",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5097496be115c484806b"
+   },
+   {
+      "name": "Baby Mario",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5041496be115c4848062"
+   },
+   {
+      "name": "Baby Luigi",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5041496be115c4848062"
+   },
+   {
+      "name": "Baby Peach",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a502f496be115c4848060"
+   },
+   {
+      "name": "Baby Daisy",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a502f496be115c4848060"
+   },
+   {
+      "name": "Baby Rosalina",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5038496be115c4848061"
+   },
+   {
+      "name": "Metal Mario",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a50ab496be115c484806e"
+   },
+   {
+      "name": "Pink Gold Peach",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a50ab496be115c484806e"
+   },
+   {
+      "name": "Wario",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a50a5496be115c484806d"
+   },
+   {
+      "name": "Waluigi",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a509f496be115c484806c"
+   },
+   {
+      "name": "Donkey Kong",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a509f496be115c484806c"
+   },
+   {
+      "name": "Bowser",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a50b1496be115c484806f"
+   },
+   {
+      "name": "Dry Bones",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5041496be115c4848062"
+   },
+   {
+      "name": "Bowser Jr",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a504a496be115c4848063"
+   },
+   {
+      "name": "Dry Bowser",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a50a5496be115c484806d"
+   },
+   {
+      "name": "Lemmy",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5038496be115c4848061"
+   },
+   {
+      "name": "Larry",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5062496be115c4848065"
+   },
+   {
+      "name": "Wendy",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a505a496be115c4848064"
+   },
+   {
+      "name": "Ludwig",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5090496be115c484806a"
+   },
+   {
+      "name": "Iggy",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5086496be115c4848069"
+   },
+   {
+      "name": "Roy",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a509f496be115c484806c"
+   },
+   {
+      "name": "Morton",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a50b1496be115c484806f"
+   },
+   {
+      "name": "Inkling Girl",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5069496be115c4848066"
+   },
+   {
+      "name": "Inkling Boy",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5080496be115c4848068"
+   },
+   {
+      "name": "Link",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5097496be115c484806b"
+   },
+   {
+      "name": "Villager (male)",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5080496be115c4848068"
+   },
+   {
+      "name": "Villager (female)",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5069496be115c4848066"
+   },
+   {
+      "name": "Isabelle",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a505a496be115c4848064"
+   },
+   {
+      "name": "Mii (Light)",
+      "size": "Small",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5041496be115c4848062"
+   },
+   {
+      "name": "Mii (Medium)",
+      "size": "Medium",
+      "driverModel": "http://localhost:3000/driverModel/5c8a5090496be115c484806a"
+   },
+   {
+      "name": "Mii (Heavy)",
+      "size": "Large",
+      "driverModel": "http://localhost:3000/driverModel/5c8a50b1496be115c484806f"
+   }
+]
